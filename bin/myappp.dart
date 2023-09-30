e1b31077373c2c221c205fb99c141fcc1c05fade
Dasha <dashabella1@gmail.com>
2023-09-30 10:06:46 +0500

    first

---
+++ bin/myappp.dart
@@ -0,0 +1,5 @@
+import 'package:myappp/myappp.dart' as myappp;
+
+void main(List<String> arguments) {
+  print('Hello world: ${myappp.calculate()}!');
+}
---
+++ test/myappp_test.dart
@@ -0,0 +1,8 @@
+import 'package:myappp/myappp.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
